62054b3fd3fd356809a566532efe5cab1d776cb8
rudotim <16530548+rudotim@users.noreply.github.com>
2025-08-31 17:14:08 -0400

    - added new pages

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const CustomerPage = () => {
+  return <div>Customers</div>;
+};
+
+export default CustomerPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const InvoicePage = () => {
+  return <div>Invoices</div>;
+};
+
+export default InvoicePage;
