14ac66465c4ef0ca9fa88d2b0d4d4974ed5ef308
rudotim <16530548+rudotim@users.noreply.github.com>
2025-10-01 17:10:41 -0400

    - database changes to normalize all sql fields to lowercase and some other fixes

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -43,15 +43,15 @@ export async function fetchRevenue(): Promise<Revenue[]> {
     //sql.query<Revenue[]>(`SELECT * FROM invoice_history limit 10`);
     sql.query<Revenue[]>(
       `
-select YEAR(Date) as "Date", sum(Amount) as "Amount", count(*) from new_invoice_history
-WHERE Date is not NULL
-group by Year(Date)
-order by Date desc
+select YEAR(date) as "Date", sum(amount) as "Amount", count(*) from invoice_history
+WHERE date is not NULL
+group by Year(date)
+order by date desc
 limit 10
       `
     );
 
-    console.log("fetchRevenue fetch completed after 3 seconds:");
+    console.log("[dashboard]fetchRevenue fetch completed after 3 seconds:");
 
     return data[0].reverse();
   } catch (error) {
@@ -61,29 +61,48 @@ limit 10
 }
 
 export async function fetchLatestInvoices(): Promise<LatestInvoice[]> {
-  console.log("fetchLatestInvoices fetch completed after 3 seconds:");
+  console.log(
+    "[dashboard]fetchLatestInvoices fetch completed after 3 seconds:"
+  );
 
   try {
     const data = await sql.query<LatestInvoice[]>(`
       SELECT
-	i.amount,
-	m.FirstName,	
-	m.email,
+	inv.amount,
+  inv.payment,
+  inv.description,
+  CASE WHEN inv.payment - inv.amount = inv.payment THEN
+    inv.payment
+  WHEN inv.payment - ABS(inv.amount) < 0 THEN	
+	  ABS(inv.payment - inv.amount)
+  WHEN inv.payment - ABS(inv.amount) = 0 THEN	  
+      inv.payment
+  END as "final",
+  CASE WHEN inv.payment - inv.amount = inv.payment THEN
+    "paid_to"
+  WHEN inv.payment - ABS(inv.amount) < 0 THEN
+    "owed" 
+  ELSE
+      "paid"
+  END as "status",  
+	concat(m.first_name, " ", m.last_name) as name,	
   "/profile.jpg" as image_url,
-	i.invoice_id
+	inv.id
 FROM
-	invoices i
+	invoices inv
 JOIN membership m ON
-	i.MembershipID = m.MembershipID
+	inv.membership_id = m.membership_id
 ORDER BY
-	i.date DESC
+	inv.date DESC
 LIMIT 5      
     `);
     const latestInvoices = data[0].map((invoice) => ({
       ...invoice,
-      amount: formatCurrency(Number(invoice.amount)),
+      final: formatCurrency(Number(invoice.final)),
+      //payment: formatCurrency(Number(invoice.payment)),
     }));
     return latestInvoices;
+    //return data[0];
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch the latest invoices.");
@@ -91,15 +110,24 @@ LIMIT 5
 }
 
 export async function fetchCardData() {
-  console.log("fetchCardData fetch completed after 3 seconds:");
+  console.log("[dashboard]fetchCardData fetch completed after 3 seconds:");
 
   try {
     const customerCountPromise = await sql.query(
-      `SELECT COUNT(*) as "count" FROM membership`
+      `SELECT COUNT(*) as "count" FROM membership
+      WHERE lower(status) = "active";`
     );
-    const invoiceStatusPromise = await sql.query(`
-      select count(*) as "count", (sum(Amount) - sum(Payment))/100 as "pending", sum(Payment) as "paid"
-      FROM invoices
+    const invoiceStatusPromise = await sql.query(`SELECT 
+      count(*) as "count", 
+      sum(p.pending) as "pending", 
+      sum(p.paid) as "paid"
+      FROM (
+        SELECT 
+        (sum(amount) - sum(payment)) as "pending", 
+        sum(payment) as "paid"
+        FROM invoices
+        GROUP BY membership_id
+      ) p;
     `);
 
     const data = await Promise.all([
@@ -109,9 +137,11 @@ export async function fetchCardData() {
 
     const numberOfCustomers = Number((data[0][0] as any)[0].count ?? 0);
     const numberOfInvoices = Number((data[1][0] as any)[0].count ?? 0);
-    const totalPaidInvoices = formatCurrency((data[1][0] as any)[0].paid ?? 0);
+    const totalPaidInvoices = formatCurrency(
+      Number((data[1][0] as any)[0].paid ?? 0)
+    );
     const totalPendingInvoices = formatCurrency(
-      (data[1][0] as any)[0].pending ?? 0
+      Number((data[1][0] as any)[0].pending ?? 0)
     );
 
     return {
@@ -133,49 +163,51 @@ export async function fetchFilteredInvoices(
 ) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
+  console.log("[invoices] fetchFilteredInvoices");
+
   try {
-    const invoices = await sql.query<InvoicesTable[]>(`
+    const data = await sql.query<InvoicesTable[]>(`
       select 
-      inv.invoice_id as "id",
-      inv.MembershipID as "customer_id", 
-        inv.Amount as "amount",
-        inv.Date as "date",
-        "paid" as "status",
-        m.FirstName as "name",
-        m.Email as "email",
+      inv.id as "id",
+      inv.membership_id as "customer_id", 
+        inv.amount,
+        inv.payment,
+        inv.description,
+  CASE WHEN inv.payment - inv.amount = inv.payment THEN
+    inv.payment
+  WHEN inv.payment - ABS(inv.amount) < 0 THEN	
+	  ABS(inv.payment - inv.amount)
+  WHEN inv.payment - ABS(inv.amount) = 0 THEN	  
+      inv.payment
+  END as "final",
+  CASE WHEN inv.payment - inv.amount = inv.payment THEN
+    "paid_to"
+  WHEN inv.payment - ABS(inv.amount) < 0 THEN
+    "owed" 
+  ELSE
+      "paid"
+  END as "status",        
+        inv.date as "date",        
+        concat(m.first_name, ' ', m.last_name) as "name",
+        m.email as "email",
         "/profile.jpg" as "image_url"
       FROM invoices inv
       LEFT JOIN membership m
-      ON m.MembershipID = inv.MembershipID
+      ON m.membership_id = inv.membership_id
       WHERE 
-        m.FirstName like "${`%${query}%`}" OR
+        m.first_name like "${`%${query}%`}" OR
+        m.last_name like "${`%${query}%`}" OR
         m.email like "${`%${query}%`}"
-      ORDER BY inv.Date DESC
+      ORDER BY inv.date DESC
         limit ${ITEMS_PER_PAGE} OFFSET ${offset}
     `);
 
-    /*
-      SELECT
-        invoices.id,
-        invoices.amount,
-        invoices.date,
-        invoices.status,
-        customers.name,
-        customers.email,
-        customers.image_url
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      WHERE
-        customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`} OR
-        invoices.amount::text ILIKE ${`%${query}%`} OR
-        invoices.date::text ILIKE ${`%${query}%`} OR
-        invoices.status ILIKE ${`%${query}%`}
-      ORDER BY invoices.date DESC
-      LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-    */
+    const invoices = data[0].map((invoice) => ({
+      ...invoice,
+      final: formatCurrency(Number(invoice.final)),
+    }));
 
-    return invoices[0];
+    return invoices;
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch invoices.");
@@ -188,11 +220,13 @@ export async function fetchInvoicesPages(query: string) {
       select count(*) as 'count'
       FROM invoices inv
       LEFT JOIN membership m
-      ON m.MembershipID = inv.MembershipID
+      ON m.membership_id = inv.membership_id
       WHERE 
-        m.FirstName like "${`%${query}%`}" OR
+        m.first_name like "${`%${query}%`}" OR
         m.email like "${`%${query}%`}"
   `);
+
+    console.log("[invoices] fetchInvoicePages");
 
     return Math.ceil(Number((data[0] as any)[0].count) / ITEMS_PER_PAGE);
   } catch (error) {
@@ -205,13 +239,15 @@ export async function fetchInvoiceById(id: string) {
   try {
     const data = await sql.query<InvoiceForm[]>(`
       SELECT
-        invoice_id as "id",
-        MembershipID as "customer_id",
-        Amount as "amount",
-        Description as "status"
+        id as "id",
+        membership_id as "customer_id",
+        amount as "amount",
+        description as "status"
       FROM invoices
-      WHERE invoice_id = "${id}";
+      WHERE id = "${id}";
     `);
+
+    console.log("[invoices] fetchInvoiceById");
 
     const invoice = data[0].map((invoice) => ({
       ...invoice,
@@ -227,15 +263,16 @@ export async function fetchInvoiceById(id: string) {
 }
 
 export async function fetchCustomers() {
-  //const customers = await sql<CustomerField[]>`
   try {
     const customers = await sql.query<CustomerField[]>(`
       SELECT
-        MembershipID as "id",
-        CONCAT(LastName, ', ', FirstName) as "name"
+        membership_id as "id",
+        CONCAT(last_name, ', ', first_name) as "name"
       FROM membership
-      ORDER BY LastName ASC
+      ORDER BY last_name ASC
     `);
+
+    console.log("[membership] Fetching membership data");
 
     return customers[0];
   } catch (err) {
@@ -253,22 +290,24 @@ export async function fetchFilteredCustomers(
   try {
     const data = await sql.query<CustomersTableType[]>(`
 SELECT
-  m.MembershipID as "id",
-  m.FirstName as "name",
+  m.membership_id as "id",
+  m.first_name as "name",
   m.email,
   "/profile.jpg" as "image_url",
-  COUNT(i.MembershipID) AS total_invoices,
-  SUM(CASE WHEN i.Description = 'pending' THEN i.amount ELSE 0 END) AS total_pending,
-  SUM(CASE WHEN i.Description = 'paid' THEN i.amount ELSE 0 END) AS total_paid
+  COUNT(i.membership_id) AS total_invoices,
+  SUM(CASE WHEN i.description = 'pending' THEN i.amount ELSE 0 END) AS total_pending,
+  SUM(CASE WHEN i.description = 'paid' THEN i.amount ELSE 0 END) AS total_paid
 FROM membership m
-LEFT JOIN invoices i ON m.MembershipID = i.MembershipID
+LEFT JOIN invoices i ON m.membership_id = i.membership_id
 WHERE
-  m.FirstName LIKE "${`%${query}%`}" OR
+  m.first_name LIKE "${`%${query}%`}" OR
   m.email LIKE "${`%${query}%`}"
-GROUP BY m.MembershipID, m.FirstName, m.email
-ORDER BY m.FirstName ASC
+GROUP BY m.membership_id, m.first_name, m.email
+ORDER BY m.first_name ASC
 limit ${ITEMS_PER_PAGE} OFFSET ${offset}
 	  `);
+
+    console.log("[membership] Fetching filtered member data");
 
     const customers = data[0].map((customer) => ({
       ...customer,
@@ -289,14 +328,15 @@ export async function fetchCustomerPages(query: string) {
 select count(*) as "count"
       FROM membership m
       INNER JOIN invoices inv
-      ON m.MembershipID = inv.MembershipID      
+      ON m.membership_id = inv.membership_id      
       WHERE 
-        m.FirstName like "${`%${query}%`}" OR
+        m.first_name like "${`%${query}%`}" OR
         m.email like "${`%${query}%`}"
-      group by m.MembershipID 
+      group by m.membership_id 
 
   `);
 
+    console.log("[membership] fetching membership page count for pagination");
     return Math.ceil(Number((data[0] as any)[0].count) / ITEMS_PER_PAGE);
   } catch (error) {
     console.error("Database Error:", error);
@@ -308,24 +348,49 @@ export async function fetchDockTableData(query: string, currentPage: number) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
-    const data = await sql.query<DockTableType[]>(`
-  select d.slipNo, concat(m.firstname, " ", m.lastname) as "name",
-d.'Boat Size', d.Owed, d.Balance from membership m
-INNER join dock_2025 d ON 
-d.MembershipID = m.MembershipID
-order by SlipNo asc
-limit ${ITEMS_PER_PAGE} OFFSET ${offset}
+    const data = await sql.query<DockTableType[]>(`SELECT 
+      d.dock_id as "id", 
+      d.slip_number, 
+      concat(m.first_name, " ", m.last_name) as "name",
+      "2025" as year,
+      d.boat_size, 
+      d.owed, 
+      d.balance 
+    FROM membership m
+    INNER join dock d ON 
+      d.membership_id = m.membership_id
+    ORDER BY d.slip_number asc
+    limit ${ITEMS_PER_PAGE} OFFSET ${offset}
 	  `);
 
-    const customers = data[0].map((customer) => ({
-      ...customer,
-      owed: formatCurrency(customer.total_pending),
-      balance: formatCurrency(customer.total_paid),
+    const dock_records = data[0].map((dock_record) => ({
+      ...dock_record,
+      owed: formatCurrency(dock_record.owed),
+      balance: formatCurrency(dock_record.balance),
     }));
 
-    return customers;
+    console.log("[dock] Fetching dock table data");
+
+    return dock_records;
   } catch (err) {
     console.error("Database Error:", err);
-    throw new Error("Failed to fetch customer table.");
+    throw new Error("Failed to fetch dock table.");
+  }
+}
+
+export async function fetchDockPages(query: string) {
+  try {
+    const data = await sql.query(`SELECT
+      count(*) as "count"
+      FROM dock d
+      INNER JOIN membership m
+      ON m.membership_id = d.membership_id
+  `);
+
+    console.log("[dock] fetching dock page count for pagination");
+    return Math.ceil(Number((data[0] as any)[0].count) / ITEMS_PER_PAGE);
+  } catch (error) {
+    console.error("Database Error:", error);
+    throw new Error("Failed to fetch total number of invoices.");
   }
 }
