3c2189e8fb2ffa05847f723f4bf3090f82bbcce0
rudotim <16530548+rudotim@users.noreply.github.com>
2025-10-01 17:11:40 -0400

    - removed some unnecessary fields from being displayed

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
 import { LatestInvoice } from "@/app/lib/definitions";
 import { fetchLatestInvoices } from "@/app/lib/data";
+import InvoiceStatus from "@/app/ui/invoices/status";
+import InvoiceAmount from "@/app/ui/invoices/invoice_amount";
 
 export default async function LatestInvoices() {
   const latestInvoices = await fetchLatestInvoices();
@@ -27,26 +29,15 @@ export default async function LatestInvoices() {
                   )}
                 >
                   <div className="flex items-center">
-                    <Image
-                      src={invoice.image_url}
-                      alt={`${invoice.name}'s profile picture`}
-                      className="mr-4 rounded-full"
-                      width={32}
-                      height={32}
-                    />
                     <div className="min-w-0">
                       <p className="truncate text-sm font-semibold md:text-base">
                         {invoice.name}
                       </p>
-                      <p className="hidden text-sm text-gray-500 sm:block">
-                        {invoice.email}
-                      </p>
                     </div>
                   </div>
-                  <p
-                    className={`${lusitana.className} truncate text-sm font-medium md:text-base`}
-                  >
-                    {invoice.amount}
+                  <p className="whitespace-nowrap px-3 py-3">{invoice.final}</p>
+                  <p className="whitespace-nowrap px-3 py-3">
+                    <InvoiceStatus status={invoice.status} />
                   </p>
                 </div>
               );
--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -1,26 +1,40 @@
-import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
+import { CheckIcon, ClockIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 
 export default function InvoiceStatus({ status }: { status: string }) {
+  // let status;
+  // if (payment > 0 && payment - amount === payment) status = "paid_to";
+  // else if (payment - Math.abs(amount) < 0) status = "owed";
+  // else if (payment > 0 && payment === amount) status = "paid";
+
+  // console.log("amount:", amount, "payment:", payment, "status:", status);
+
   return (
     <span
       className={clsx(
-        'inline-flex items-center rounded-full px-2 py-1 text-xs',
+        "inline-flex items-center rounded-full px-2 py-1 text-xs",
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
-          'bg-green-500 text-white': status === 'paid',
-        },
+          "bg-red-400 text-white": status === "owed",
+          "bg-green-500 text-white": status === "paid",
+          "bg-blue-500 text-white": status === "paid_to",
+        }
       )}
     >
-      {status === 'pending' ? (
+      {status === "owed" ? (
         <>
-          Pending
-          <ClockIcon className="ml-1 w-4 text-gray-500" />
+          Owe
+          <ClockIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
-      {status === 'paid' ? (
+      {status === "paid" ? (
         <>
           Paid
+          <CheckIcon className="ml-1 w-4 text-white" />
+        </>
+      ) : null}
+      {status === "paid_to" ? (
+        <>
+          Pay Out
           <CheckIcon className="ml-1 w-4 text-white" />
         </>
       ) : null}
