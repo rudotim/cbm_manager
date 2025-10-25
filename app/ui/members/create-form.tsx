3e4d2e62df5ef20720a8d8659eb22bd5e040ec22
rudotim <16530548+rudotim@users.noreply.github.com>
2025-10-25 10:52:34 -0400

    - fixed error when creating new member due to undefined radio value

--- app/ui/members/create-form.tsx
+++ app/ui/members/create-form.tsx
@@ -129,6 +129,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                     type="radio"
                     name="membership_type"
                     value="Stock"
+                    defaultChecked
                     onChange={onNewMembershipType}
                     className="relative size-4 appearance-none rounded-full border border-gray-300 bg-white before:absolute before:inset-1 before:rounded-full before:bg-white not-checked:before:hidden checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:border-gray-300 disabled:bg-gray-100 disabled:before:bg-gray-400 forced-colors:appearance-auto forced-colors:before:hidden"
                   />
