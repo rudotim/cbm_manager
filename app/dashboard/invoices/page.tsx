d91e9ea1424200323e55bc10533d28fc1be75888
rudotim <16530548+rudotim@users.noreply.github.com>
2025-12-29 14:41:06 -0500

    - reset invoices button is now red and the text is visible

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -25,7 +25,7 @@ export default async function Page(props: {
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
-        <CreateInvoice />
+        {/*<CreateInvoice />*/}
         <ResetInvoices />
       </div>
       {
--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -41,9 +41,13 @@ export function DeleteInvoice({ id }: { id: string }) {
 export function ResetInvoices() {
   return (
     <form action={resetInvoices}>
-      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Reset Invoices</span>
-        <TrashIcon className="w-5" />
+      <button
+        type="submit"
+        title="Reset Invoices"
+        className="flex h-10 items-center rounded-lg bg-red-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+      >
+        <span className="hidden md:block">Reset Invoices</span>
+        <TrashIcon className="h-5 md:ml-2;" />
       </button>
     </form>
   );
