6b0d36bec825b2c14886e40ba875838c4cad8009
rudotim <16530548+rudotim@users.noreply.github.com>
2026-01-03 23:15:26 -0500

    - fixed issue where button was submitting form when it shouldn't have been

--- app/ui/invoices/reset-invoice-button.tsx
+++ app/ui/invoices/reset-invoice-button.tsx
@@ -17,9 +17,9 @@ export function ResetInvoices() {
   };
 
   return (
-    <form action={resetInvoices}>
+    <form>
       <button
-        //type="submit"
+        type="button"
         onClick={handleOpenDialog}
         title="Reset Invoices"
         className="flex h-10 items-center rounded-lg bg-red-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
