ebc848fbd3ac1428cea9ed2cb97d3e54b547f02c
rudotim <16530548+rudotim@users.noreply.github.com>
2026-02-12 22:30:48 -0500

    - renamed Customer to Member

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -21,7 +21,7 @@ export default async function InvoicesTable({
             <thead className="rounded-lg text-left text-sm font-normal">
               <tr>
                 <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                  Customer
+                  Member
                 </th>
                 <th scope="col" className="px-3 py-5 font-medium">
                   Amount
