29181e084ccc8e742e94b2aca1ffec5f06e6e8ca
rudotim <16530548+rudotim@users.noreply.github.com>
2026-02-18 20:24:53 -0500

    - added logic to force requery of settings data

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -2,6 +2,9 @@ import Form from "@/app/ui/settings/edit-form";
 import Breadcrumbs from "@/app/ui/members/breadcrumbs";
 import { fetchSettings } from "@/app/lib/data";
 
+// Stop the query from being cached
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const settings = await fetchSettings();
 
